59a4b95c618d537ba2b1b39ff12f627a281133bf
Verwoerd <thomas@keystone-strategic.com>
2022-12-01 06:22:34 +0100

    Fix tests not showing

--- shared/src/main/kotlin/ExampleTest.kt
+++ shared/src/main/kotlin/ExampleTest.kt
@@ -6,7 +6,7 @@ abstract class ExampleTest(examples: Int, part: Int, func: (BufferedReader) -> A
   {
     "Part $part" - {
       (1..examples).map {
-        "test $it" - {
+        "test $it" {
           val input = this::class.java.getResourceAsStream("/part$part/example$it.input").bufferedReader()
           val output = this::class.java.getResourceAsStream("/part$part/example$it.output").bufferedReader().readText()
           val result = func(input)
